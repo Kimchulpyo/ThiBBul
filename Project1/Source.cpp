666f21e3519e6720574c9d985a266b762484315f
김철표 <kim.chulpyo@sualab.com>
2017-02-27 09:32:51 +0900

    chapter1: array, function

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
+#include <array>
 #include "Database.h"
 using namespace std;
 using namespace Records;
 
+// Chapter1 functions
+void debugging(int n1, int n2);
+
 int main(void) {
 
-	// version 0.000000001
+	// version 0.000000001 ~page 89
 	//cout << "Testing the Employee class." << endl;
 	//Employee emp;
 	//emp.setFirstName("Chulpyo");
@@ -19,27 +23,45 @@ int main(void) {
 	//emp.hire();
 	//emp.display();
 
-	Database myDB;
-	Employee& emp1 = myDB.addEmplyee("Chulpyo", "Kim");
-	emp1.setSalary(100000000);
-	emp1.fire();  // Oh!!! god..
+	// version 0.000000002 ~page 93
+	//Database myDB;
+	//Employee& emp1 = myDB.addEmplyee("Chulpyo", "Kim");
+	//emp1.setSalary(100000000);
+	//emp1.fire();  // Oh!!! god..
 
-	Employee& emp2 = myDB.addEmplyee("Greg", "Wallis");
-	emp2.setSalary(100000);
+	//Employee& emp2 = myDB.addEmplyee("Greg", "Wallis");
+	//emp2.setSalary(100000);
 
-	Employee& emp3 = myDB.addEmplyee("Scott", "Kleper");
-	emp3.setSalary(10000);
-	emp3.promte();
+	//Employee& emp3 = myDB.addEmplyee("Scott", "Kleper");
+	//emp3.setSalary(10000);
+	//emp3.promte();
 
-	cout << "all employees: " << endl;
-	cout << endl;
-	myDB.displayAll();
+	//cout << "all employees: " << endl;
+	//cout << endl;
+	//myDB.displayAll();
 
-	cout << endl;
-	cout << "Current employees: " << endl;
+	//cout << endl;
+	//cout << "Current employees: " << endl;
 
-	cout << endl;
-	myDB.displayCurrent();
+	//cout << endl;
+	//myDB.displayCurrent();
+		
+	// Chapter 1 array
+
+	array<int, 3> arr = { 2, 3, 4 };
+	cout << "Array size= " << arr.size() << endl;
+	for (auto i : arr) {
+		cout << i << endl;
+	}
+	
+	// Chapter 1 function
+	// print function name
+	debugging(1, 2);
 
 	return 0;
+}
+
+void debugging(int n1, int n2) {
+	cout << "Enter function >> " << __func__ << endl;
+	cout << "param : " << n1 << " " << n2 << endl;
 }
